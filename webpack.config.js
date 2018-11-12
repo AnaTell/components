99af0a2bb65e93a3f159b8e46ec5ceb38285fdd5
Ana Tellez <ana.tellez.r@gmail.com>
2018-11-12 10:04:54 -0600

    buttons

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
-module.exports = {
-  entry: path.resolve(__dirname, "./lib/wagon_race.js"),
-  output: {
-    filename: "javascript/game_board.js"
-  },
-  devtool: "sourcemap"
-};
+// module.exports = {
+//   entry: path.resolve(__dirname, "./lib/wagon_race.js"),
+//   output: {
+//     filename: "javascript/game_board.js"
+//   },
+//   devtool: "sourcemap"
+// };
